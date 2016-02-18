2259238dd44a61c8c55b5ea0e386785b477d57b6
F.Cesni.Maldonado <sigeusb@hotmail.com>
2016-02-18 11:02:28 -0600

    View edit & Gender Testing value

--- scripts/controllers/view.js
+++ scripts/controllers/view.js
@@ -12,20 +12,20 @@ eventica.controller('ViewCtrl',function($scope,Session,EventicaLogin){
 	}
 
 	$scope.users=[
-		{name:'Armando',descripcion:"Informacion General del BA",photo:"1.jpg",age:"29"},
-		{name:'Alvin',descripcion:"Informacion General del BA",photo:"2.jpg",age:"34"},
-		{name:'Alberto',descripcion:"Informacion General del BA",photo:"3.jpg",age:"45"},
-		{name:'Benito',descripcion:"Informacion General del BA",photo:"4.jpg",age:"19"},
-		{name:'Bernardo',descripcion:"Informacion General del BA",photo:"5.jpg",age:"23"},
-		{name:'Carlos',descripcion:"Informacion General del BA",photo:"6.jpg",age:"24"},
-		{name:'Daniel',descripcion:"Informacion General del BA",photo:"7.jpg",age:"25"},
-		{name:'Armando',descripcion:"Informacion General del BA",photo:"1.jpg",age:"28"},
-		{name:'Alvin',descripcion:"Informacion General del BA",photo:"2.jpg",age:"30"},
-		{name:'Alberto',descripcion:"Informacion General del BA",photo:"3.jpg",age:"32"},
-		{name:'Benito',descripcion:"Informacion General del BA",photo:"4.jpg",age:"16"},
-		{name:'Bernardo',descripcion:"Informacion General del BA",photo:"5.jpg",age:"25"},
-		{name:'Carlos',descripcion:"Informacion General del BA",photo:"6.jpg",age:"31"},
-		{name:'Daniel',descripcion:"Informacion General del BA",photo:"7.jpg",age:"20"}
+		{name:'Armando',descripcion:"Informacion General del BA",photo:"1.jpg",gender:'male',age:"29"},
+		{name:'Alvin',descripcion:"Informacion General del BA",photo:"2.jpg",gender:'male',age:"34"},
+		{name:'Alberto',descripcion:"Informacion General del BA",photo:"3.jpg",gender:'male',age:"45"},
+		{name:'Benito',descripcion:"Informacion General del BA",photo:"4.jpg",gender:'male',age:"19"},
+		{name:'Bernardo',descripcion:"Informacion General del BA",photo:"5.jpg",gender:'male',age:"23"},
+		{name:'Carlos',descripcion:"Informacion General del BA",photo:"6.jpg",gender:'male',age:"24"},
+		{name:'Daniel',descripcion:"Informacion General del BA",photo:"7.jpg",gender:'male',age:"25"},
+		{name:'Armando',descripcion:"Informacion General del BA",photo:"1.jpg",gender:'male',age:"28"},
+		{name:'Alvin',descripcion:"Informacion General del BA",photo:"2.jpg",gender:'male',age:"30"},
+		{name:'Alberto',descripcion:"Informacion General del BA",photo:"3.jpg",gender:'male',age:"32"},
+		{name:'Benito',descripcion:"Informacion General del BA",photo:"4.jpg",gender:'male',age:"16"},
+		{name:'Bernardo',descripcion:"Informacion General del BA",photo:"5.jpg",gender:'male',age:"25"},
+		{name:'Carlos',descripcion:"Informacion General del BA",photo:"6.jpg",gender:'male',age:"31"},
+		{name:'Daniel',descripcion:"Informacion General del BA",photo:"7.jpg",gender:'male',age:"20"}
 	];
 	$scope.criterio;
 
